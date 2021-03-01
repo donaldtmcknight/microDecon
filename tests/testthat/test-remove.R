b07072966fe704873271a4b5babb8353da926cf0
donaldtmcknight <donald.mcknight@my.jcu.edu.au>
2021-03-01 14:46:23 +1000

    fixed test

--- tests/testthat/test-remove.R
+++ tests/testthat/test-remove.R
@@ -1,6 +1,6 @@
 test_that("load dataset", {
   
-  load( sprintf("%s/data/Example_1.Rdata",getwd()))
+  load( sprintf("../../data/Example_1.Rdata",getwd()))
   
   expect_type(Example_1,"list")
   expect_equal(dim(Example_1),c(6,8))
