441aad2e535c333d17f0723e846dca8fd067f064
donaldtmcknight <donald.mcknight@my.jcu.edu.au>
2021-03-01 14:56:21 +1000

    updated directory

--- tests/testthat/test-remove.R
+++ tests/testthat/test-remove.R
@@ -1,9 +1,12 @@
 test_that("load dataset", {
   
-  load( sprintf("../../data/Example_1.Rdata",getwd()))
+  load( system.file("data/Example_1.RData", package = "microDecon"))
   
   expect_type(Example_1,"list")
   expect_equal(dim(Example_1),c(6,8))
   
   
 })
+
+
+#run checks using devtools::test()
